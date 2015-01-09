f346f7a95a8a2fff4eb5a9b4a537bd5f9fdfbb78
di wu <di.wu@me.com>
2015-01-09 22:40:29 +0800

    Add animation

--- DWSwitchDemo/ViewController.swift
+++ DWSwitchDemo/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var ibSwitch: DWAnimationSwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let mySwitch1 = DWAnimationSwitch(frame: CGRectMake(0, 0, 100, 50))
+        mySwitch1.center = CGPointMake(self.view.bounds.size.width * 0.5, self.view.bounds.size.height * 0.5 - 80)
+        mySwitch1.addTarget(self, action: "switchChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        mySwitch1.offImage = UIImage(named: "cross")
+        mySwitch1.onImage = UIImage(named: "check")
+        mySwitch1.onTintColor = UIColor.blueColor()
+        mySwitch1.isRounded = false;
+        self.view.addSubview(mySwitch1)
     }
-
+    func switchChanged(sender: DWAnimationSwitch) {
+        println("Changed value to: \(sender.on)")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
